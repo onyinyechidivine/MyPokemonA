6654308dfa377b2512a20b674be9100c8337cecc
onyinyechidivine <ajachukwuonyinyechi@gmail.com>
2023-08-05 15:54:18 -0700

    updated axios

--- src/containers/PokemonDetails.js
+++ src/containers/PokemonDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+import axios from "axios"
 import { POKEMON_API_URL } from "../config";
 import { ClipLoader } from "react-spinners";
 import { useMatch } from "react-router-dom";
